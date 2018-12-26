5e5475878c091f23da34a6517aa5b4ea93e3d820
unknown <hoyoooo@gmail.com>
2018-12-26 14:09:49 +0300

    pytask3b

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length = 120)
+    describtion = models.TextField()
+    opening_time = models.DateTimeField()
+    closing_time = models.DateTimeField()
+
+
+def __str__(self):
+   return self.name
